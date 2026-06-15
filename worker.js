920e2065658034f736378d2fc238df35a0fa56f6
NeoCloud <84351268+hmei7-codex@users.noreply.github.com>
2026-06-16 00:03:02 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,40 +1,7 @@
 export default {
   async fetch(request, env) {
-    try {
-      const url = new URL(request.url);
-
-      if (url.pathname === "/check") {
-        const license = url.searchParams.get("license");
-
-        if (!license) {
-          return Response.json({
-            success: false,
-            message: "license required"
-          });
-        }
-
-        const raw = await env.LICENSES.get(license);
-
-        if (!raw) {
-          return Response.json({
-            success: false,
-            message: "license not found"
-          });
-        }
-
-        return Response.json({
-          success: true,
-          license,
-          raw
-        });
-      }
-
-      return new Response("NeoCloud License Server");
-    } catch (e) {
-      return new Response(
-        "ERROR: " + e.message,
-        { status: 500 }
-      );
-    }
+    return Response.json({
+      licenses_exists: !!env.LICENSES
+    });
   }
 }
