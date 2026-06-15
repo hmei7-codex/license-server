6f4ffa1431c9319eeba1eb10b291aa568a744425
NeoCloud <84351268+hmei7-codex@users.noreply.github.com>
2026-06-16 00:44:25 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,7 @@
 export default {
   async fetch(request, env) {
-    const data = await env.LICENSES.get("test");
-
     return Response.json({
-      licenses_exists: !!env.LICENSES,
-      test_value: data
+      licenses_exists: !!env.LICENSES
     });
   }
 }
